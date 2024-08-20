b573e1080f01ce9e978da61c3841ccbc52a9025a
DanielGraham123 <dg.danielboaz@yahoo.com>
2024-08-20 16:15:53 +0100

    feat: Integrate createProject action to add new projects

--- revver123/components/AddProject.jsx
+++ revver123/components/AddProject.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { Button } from "./ui/button";
 import { Plus } from "lucide-react";
@@ -13,6 +14,7 @@ import {
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import { createProject } from "@/actions/createProject";
 
 const AddProject = () => {
   return (
@@ -30,7 +32,7 @@ const AddProject = () => {
             Create a new project to get reviews from users.
           </DialogDescription>
         </DialogHeader>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" action={createProject}>
           <div className="grid grid-cols-2 gap-4">
             <div className="flex flex-col gap-2 items-start">
               <Label htmlFor="title" className="text-right">
--- revver123/db/schema.js
+++ revver123/db/schema.js
@@ -1,8 +1,9 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 
 export const projects = pgTable('projects', {
     id: serial('id').primaryKey(),
     title: text('title'),
     url: text('url'),
     description: text('description'),
+    userId: varchar('user_id'),
 });
