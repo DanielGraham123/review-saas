d2b4606f36a4d0b3c350868dfbd8571918e39a27
DanielGraham123 <dg.danielboaz@yahoo.com>
2024-08-22 15:33:54 +0100

    feat: Update project detail page to include back button and project actions
    The code changes in project-detail.jsx add a back button to navigate to the dashboard and include project actions such as visiting the project site and embedding code. This enhancement improves the user experience by providing easy navigation and access to project-related actions directly from the project detail page.

--- revver123/app/(user)/projects/[slug]/project-detail.jsx
+++ revver123/app/(user)/projects/[slug]/project-detail.jsx
@@ -1,7 +1,52 @@
+"use client";
+import Table from "@/components/Table";
+import Link from "next/link";
 import React from "react";
+import { Globe, ChevronLeft, Code } from "lucide-react";
 
 const ProjectDetail = ({ project }) => {
-  return <div>{project?.title}</div>;
+  return (
+    <div>
+      <div>
+        <Link
+          href="/dashboard"
+          className="flex items-center text-indigo-700 mb-5 w-fit"
+        >
+          <ChevronLeft className="h-5 w-5 mr-1" />
+          <span className="text-lg">Back to projects</span>
+        </Link>
+      </div>
+      <div className="flex justify-between items-start">
+        <div className="proj-info">
+          <h1 className="text-3xl font-bold mb-3">{project.title}</h1>
+          <h2 className="text-primary-background text-xl mb-2">
+            {project.description}
+          </h2>
+        </div>
+        <div className="flex flex-col">
+          {project.url ? (
+            <Link
+              href={project.url}
+              className="underline text-indigo-700 flex items-center"
+            >
+              <Globe className="h-5 w-5 mr-1" />
+              <span className="text-lg">Visit site</span>
+            </Link>
+          ) : null}
+          <Link
+            href={`/projects/${project.slug}/instructions`}
+            className="underline text-indigo-700 flex items-center mt-2"
+          >
+            <Code className="h-5 w-5 mr-1" />
+            <span className="text-lg">Embed Code</span>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Table data={project.feedbacks} />
+      </div>
+    </div>
+  );
 };
 
 export default ProjectDetail;
